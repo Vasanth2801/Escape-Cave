4726ff20df8d88eefba145ca8141b08d144ff946
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-18 10:27:52 +0530

    Jump, Flip, Animations and Attack Logic

--- Escape Cave/Assets/Scripts/Player/Player.cs
+++ Escape Cave/Assets/Scripts/Player/Player.cs
@@ -4,9 +4,23 @@ public class Player : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 9f;
+    [SerializeField] private int facingDirection = 1;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+
+    [Header("Attack Settings")]
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private LayerMask attackLayer;
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
 
     [Header("Inputs")]
     [SerializeField] private float moveInput;
@@ -14,6 +28,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        Jump();
+
+        if(moveInput > 0 && transform.localScale.x < 0 || moveInput < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        HandleAnimations();
+
+        Attack();
     }
 
     void FixedUpdate()
@@ -24,5 +49,39 @@ public class Player : MonoBehaviour
     void Move()
     {
         rb.linearVelocity = new Vector2(moveInput * speed * Time.deltaTime,rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce * Time.deltaTime);
+        }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    void Attack()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRadius, attackLayer);
+            foreach(Collider2D hit in hitEnemies)
+            {
+                Debug.Log("Attacking the Enemy");
+            }
+        }
+    }
+
+    void HandleAnimations()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        animator.SetBool("isJumping", rb.linearVelocity.y > 0.1f);
     }
 }
